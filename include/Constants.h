a3b32ab98504350d731f1f247defece5968aafc6
Malte Quandt <malte.quandt@student.fh-kiel.de>
2022-05-15 00:31:38 +0200

    Implemented constants file
    Constants file contains all symbollic constants that are used all over the application

---
+++ include/Constants.h
@@ -0,0 +1,13 @@
+//
+// Created by malte on 5/14/2022.
+//
+
+#ifndef PHYSICS_RENDERER_CONSTANTS_H
+#define PHYSICS_RENDERER_CONSTANTS_H
+
+// Symbolic Constants needed in large parts of the codebase
+// ------------------
+#define STARTING_WINDOW_HEIGHT 600
+#define STARTING_WINDOW_WIDTH 800
+
+#endif //PHYSICS_RENDERER_CONSTANTS_H
