12d9e63d622afc9ded13d241f2c6f416a33d0b17
Malte Quandt <malte.quandt@student.fh-kiel.de>
2022-06-05 22:25:52 +0200

    Implemented empty logger library

---
+++ lib/logger/include/Logger.h
@@ -0,0 +1,11 @@
+#ifndef PHYSICS_RENDERER_LOGGER_H
+#define PHYSICS_RENDERER_LOGGER_H
+namespace phyren {
+    namespace logging {
+        class Logger {
+
+        };
+    }
+}
+
+#endif //PHYSICS_RENDERER_LOGGER_H
---
+++ lib/logger/src/Logger.cpp
@@ -0,0 +1,8 @@
+//
+// Created by malte on 6/5/2022.
+//
+
+#include "../include/Logger.h"
+
+using namespace phyren::logging;
+
